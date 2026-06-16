24768dbd981ad86b6ed4637594d3ece4732f64c8
Mariam Ahmed <mariamff11@gmail.com>
2026-06-15 22:24:57 -0230

    Styles with CSS

---
+++ movies-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+const Card = ({ title }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
+    </div>
+  );
+}
+
+export default App
