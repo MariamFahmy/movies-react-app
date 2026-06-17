3c49e38b04a8e86728b63a7784bc487a4b8d779d
Mariam Ahmed <mariamff11@gmail.com>
2026-06-16 23:21:27 -0230

    Used useState to add a Like/Unlike movie button

--- movies-react/src/App.jsx
+++ movies-react/src/App.jsx
@@ -1,25 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 const Card = ({ title }) => {
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-    </div>
-  )
-}
+    // State is not persistent across browser reloads
+    // On reloading the page, the component gets rerendered and the state
+    // returns to its initial value defined by useState's initialState
+    // parameter
+
+    /**
+     * When the state changes, React automatically re-renders the component
+     * to reflect the new data; it does not reload the page.
+     * React only re-renders the Card component that has had its state change.
+     */
+    const [hasLiked, setHasLiked] = useState(false);
+
+    return (
+      <div className="card">
+        <h2>{title}</h2>
+        <button onClick={() => setHasLiked(!hasLiked)}>
+          {hasLiked ? '❤️' : '🤍'}
+        </button>
+      </div>
+    );
+};
 
 const App = () => {
   return (
     <div className="card-container">
       <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar"/>
-      <Card title="The Lion King"/>
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
     </div>
   );
-}
+};
 
-export default App
+export default App;
